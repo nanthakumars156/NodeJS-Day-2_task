4b22eea9dcd7e300dd451dce1629453b6c02e345
nanthakumars156 <nanthakumars156@gmail.com>
2024-12-03 22:20:05 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 require ('dotenv').config();
 const webServer = express();
-
+const PORT = 3000;
 // to convert into json
 webServer.use(bodyParser.json());
 
